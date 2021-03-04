09dd4bfd86dae5e40c68b0f7c0fdbdfa3c147827
Gostraightonly <76559328+Gostraightonly@users.noreply.github.com>
2021-03-05 04:00:15 +0900

    Update dbconnect.php

--- web_study/새 폴더/dbconnect.php
+++ web_study/새 폴더/dbconnect.php
@@ -1,8 +1,8 @@
 <?php
 $mysql_hostname = "localhost";
-$mysql_user = "brian7012";
-$mysql_password = "skdmldnpq1!";
-$mysql_database = "brian7012";
+$mysql_user = "##";
+$mysql_password = "##";
+$mysql_database = "##";
 
 $db = new mysqli($mysql_hostname, $mysql_user, $mysql_password, $mysql_database) or die("db connect error");
  ?>
